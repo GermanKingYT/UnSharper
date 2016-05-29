7f858dfb109a6c3a3a77d33aa01029ca15d438f7
XenocodeRCE <samjam5376@gmail.com>
2016-05-29 20:47:59 +0200

    V1.0
    Basic strings decryption for this very basic obfuscator

---
+++ SeeUnsharpDeober/Core/Helper.cs
@@ -0,0 +1,135 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeUnsharpDeober.Core
+{
+    class Helper
+    {
+        public static ModuleDefMD asm;
+
+        public static MethodDef GetDecryptType(ModuleDefMD asmodule)
+        {
+            //
+            var module_types = asmodule.Types;
+            asm = asmodule;
+            //
+            foreach (TypeDef type in module_types)
+            {
+                if (type.Name == "<Module>") continue;
+                foreach (MethodDef method in type.Methods)
+                {
+                    if (!method.IsConstructor) continue;
+                    var instrsdec = method.Body.Instructions;
+                    if (instrsdec.Count < 5) continue;
+                    for (int i = 0; i < instrsdec.Count; i++)
+                    {
+                        if (method.Body.Instructions[0].OpCode == OpCodes.Ldstr && method.Body.Instructions[1].OpCode == OpCodes.Call 
+                            && method.Body.Instructions[2].OpCode == OpCodes.Stsfld)
+                        {
+                           
+                            return method;
+                        }
+                      
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static int Extract_string_value(MethodDef method_)
+        {
+            //
+            var module_types = asm.Types;
+            var count = 0;
+            //
+            var instrsdec = method_.Body.Instructions;
+            for (int i = 0; i < instrsdec.Count; i++)
+            {
+                if (method_.Body.Instructions[i].OpCode == OpCodes.Ldstr)
+                {
+                    string str_to_decrypt = (string)method_.Body.Instructions[i].Operand;
+                    string decrypted_obj = decrypt_strings(str_to_decrypt);
+                    if (method_.Body.Instructions[i + 2].OpCode == OpCodes.Stsfld)
+                    {
+                        var my_field = (FieldDef)method_.Body.Instructions[i + 2].Operand;
+                        if(ReplaceString(decrypted_obj, my_field))
+                        {
+                            Console.WriteLine("[+] " + decrypted_obj + " : " + my_field.Name);
+                            count++;
+                        }
+                    }
+                }
+
+            }
+            return count;
+        }
+
+
+        public static bool ReplaceString(string decrypted_string, FieldDef field_)
+        {
+            //
+            var module_types = asm.Types;
+            //
+            foreach (TypeDef type in module_types)
+            {
+                foreach (MethodDef method in type.Methods)
+                {
+                    var instrsdec = method.Body.Instructions;
+                    for (int i = 0; i < instrsdec.Count; i++)
+                    {
+                        if (method.Body.Instructions[i].OpCode == OpCodes.Ldsfld && method.Body.Instructions[i].Operand != null)
+                        {
+                            try
+                            {
+                                var got_field = (FieldDef)method.Body.Instructions[i].Operand;
+                                if (got_field.MDToken == field_.MDToken)
+                                {
+                                    method.Body.Instructions[i].OpCode = OpCodes.Ldstr;
+                                    method.Body.Instructions[i].Operand = decrypted_string;
+                                    return true;
+                                }
+                            }
+                            catch (Exception)
+                            {
+
+                                //
+                            }
+                                                   
+                        }
+
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static string decrypt_strings(string str)
+        {
+	        char[] array = str.ToCharArray();
+                byte[] array2 = new byte[array.Length * 2];
+	        for (int i = 0; i<array.Length; i++)
+	        {
+		        array2[i * 2] = (byte)(array[i] >> 8);
+		        array2[i * 2 + 1] = (byte)(array[i] & 'ÿ');
+	        }
+            Array.Reverse(array2);
+	        int num = array2.Length - 2;
+	        if (array2[0] == 1)
+	        {
+		        num++;
+	        }
+            byte[] array3 = new byte[num];
+	        for (int j = 0; j<array3.Length; j++)
+	        {
+		        array3[j] = (byte)((int)array2[j + 1] - array3.Length* 123 - j* 41);
+	        }
+	        return Encoding.UTF8.GetString(array3);
+        }
+
+    }
+}
---
+++ SeeUnsharpDeober/Program.cs
@@ -0,0 +1,97 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeUnsharpDeober
+{
+    class Program
+    {
+        static ModuleDefMD asm;
+        public static string asmpath;
+        public static int DeobedStringNumber;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(@" _____     _____ _                       ");
+            Console.WriteLine(@"|  |  |___|   __| |_ ___ ___ ___ ___ ___ ");
+            Console.WriteLine(@"|  |  |   |__   |   | .'|  _| . | -_|  _|");
+            Console.WriteLine(@"|_____|_|_|_____|_|_|__,|_| |  _|___|_|  ");
+            Console.WriteLine(@"                            |_|XenocodeRCE");
+            Console.WriteLine(@"");
+            Console.WriteLine(@"");
+            if (args == null || args.Length == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[!]Error : No file to deobfuscate ! ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadKey();
+                return;
+            }
+            else{
+                try
+                {
+                    asm = ModuleDefMD.Load(args[0]);
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("[!]Loading assembly " + asm.FullName);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    asmpath = args[0];
+                    var dec_method = Core.Helper.GetDecryptType(asm);
+                    if(dec_method != null)
+                    {
+                        Console.WriteLine("[!]Instancing decryption method : " + dec_method.FullName);
+                        Console.WriteLine("[!]Decrypting Strings ... : ");
+                        var decryptedstr = Core.Helper.Extract_string_value(dec_method);
+                        if(decryptedstr != 0)
+                        {
+                            DeobedStringNumber = decryptedstr;
+                        }
+
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine(@"[!] Successfully decrypted " + DeobedStringNumber + " strings.");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine(@"[!] Saving Module...");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        string text2 = Path.GetDirectoryName(args[0]);
+                        if (!text2.EndsWith("\\"))
+                        {
+                            text2 += "\\";
+                        }
+                        string path = text2 + Path.GetFileNameWithoutExtension(args[0]) + "_patched" +
+                                      Path.GetExtension(args[0]);
+                        var opts = new ModuleWriterOptions(asm);
+                        opts.Logger = DummyLogger.NoThrowInstance;
+                       
+                        asm.Write(path, opts);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(@"[!] Saved ! ");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("[!]Error : Cannot find the decryption method !");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("[!]Error : Cannot load the file. Make sure it's a valid .NET file !");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.ReadKey();
+                    return;
+                }
+            }
+        }
+    }
+}
